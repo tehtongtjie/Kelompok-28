3fd9569c0224c8eacb515ff845353ea8944fdeda
Nagisa1953 <myemail@kandarifqi3@gmail.com>
2024-12-14 20:38:23 +0800

    edge

--- edge.java
+++ edge.java
@@ -1,2 +1,13 @@
-public class edge {
+public class Edge {
+    Node from;
+    Node to;
+    int weight;
+    Edge nextEdge;
+
+    public Edge(Node from, Node to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+        this.nextEdge = null;
+    }
 }
