4d3dda010729d8cd137115f2d4450ae83ebaa7cc
tehtongtjie <f1d02310071@student.unram.ac.id>
2024-12-15 18:40:30 +0800

    qiqi memberi sedikit perubahan

--- main.java
+++ main.java
@@ -3,11 +3,16 @@ public class main {
         Linkedlist list = new Linkedlist();
 
         // Tambahkan beberapa data
-        list.add("Alice", "123", 10);
-        list.add("Bob", "456", 25);
-        list.add("Charlie", "789", 17);
-        list.add("David", "012", 30);
+        list.add("Wadis", "123", 10);
+        list.add("Irfan", "456", 25);
+        list.add("Farid", "789", 17);
+        list.add("Ocha", "012", 11);
         list.add("Budi", "345", 5);
+        list.add("Kanda", "678", 20);
+        list.add("Rayan", "901", 8);
+        list.add("Rafi", "234", 22);
+        list.add("Asep", "567", 15);
+        list.add("Adi", "890", 12);
 
         System.out.println("Semua Mahasiswa:");
         list.printAll();
