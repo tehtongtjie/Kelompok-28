4be37a50c32a636fd66ed2bd4ef8794f3cd68552
kamiliarosada0104@gmail.com <kamiliarosada0104@gmail.com>
2024-12-15 23:01:45 +0800

    Diubah Ocha

--- Node.java
+++ Node.java
@@ -1,17 +1,18 @@
 public class Node {
-    String name, nim;
+    String nama;
+    String nim;
     int jarak;
     Node prev, next;
 
-    public Node(String name, String nim, int jarak) {
-        this.name = name;
+    public Node(String nama, String nim, int jarak) {
+        this.nama = nama;
         this.nim = nim;
         this.jarak = jarak;
 
     }
 
     public void print() {
-        System.out.println("Nama Mahasiswa: " + name);
+        System.out.println("Nama Mahasiswa: " + nama);
         System.out.println("NIM: " + nim);
         System.out.println("Jarak: " + jarak + " km");
         System.out.println("================================");
